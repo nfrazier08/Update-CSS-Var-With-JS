2f9176ddfbb8c13998ff90f73039250c5f6dd900
neliza <nicolasaelf64@gmail.com>
2019-02-09 16:54:37 -0700

    more notes of js functions describing what each line of code is doing

--- script.js
+++ script.js
@@ -1,17 +1,28 @@
 window.onload = function(){
 
-//query selector returns node list
+//Query selector returns node list not an array
+//Select all inputs on the page
 const inputs = document.querySelectorAll('.controls input');
 
+//Here we are updating the CSS variables
 function handleUpdate() {
+    //Grab suffix var (specifically px)
+    //This comes from the 'data-sizing' added to the blur and spacing inputs
+    //We also want to add || "" to capture the change of the background image color because it does not have a suffix
+    //..As the base color is simply a hex code that is the value and doesn't need px appended to it
     const suffix = this.dataset.sizing ||"";
 
+    //Select variables and set property to the value of the input 
+    //We also need to add suffix to the end of it to append the pixels
     document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix)
 
     console.log(this.name);
 
 }
 
+//Listen for change event on inputs and call handleUpdate function
+//Add event listener to call handleUpdate function on the change of the inputs, but also when the input bar
+//..is dragged around alot, which is where the mousemove event comes into play here
 inputs.forEach(input => input.addEventListener('change', handleUpdate));
 inputs.forEach(input => input.addEventListener('mousemove', handleUpdate))
 
