bb80acdaf129972d5c3536322496b658c316d7e6
neliza <nicolasaelf64@gmail.com>
2019-02-09 16:42:14 -0700

    added complete js functions to change values on input

---
+++ script.js
@@ -0,0 +1,19 @@
+window.onload = function(){
+
+//query selector returns node list
+const inputs = document.querySelectorAll('.controls input');
+
+function handleUpdate() {
+    const suffix = this.dataset.sizing ||"";
+
+    document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix)
+
+    console.log(this.name);
+
+}
+
+inputs.forEach(input => input.addEventListener('change', handleUpdate));
+inputs.forEach(input => input.addEventListener('mousemove', handleUpdate))
+
+
+};
